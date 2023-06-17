3bf11360552afdc172c215d9bb92a74d21886a75
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2023-06-17 13:19:59 +0400

    Add files via upload

---
+++ Abb/js/app.js
@@ -0,0 +1,8 @@
+let inp = document.querySelector('.inp')
+let btn = document.querySelector('.btn')
+
+btn.onclick = () =>{
+    if(inp.style = 'display: none'){
+        inp.style = 'display: inline'
+    }
+}
